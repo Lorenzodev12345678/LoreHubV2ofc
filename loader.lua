1de2d2c00eb0075a2812b19565967edd52fce990
Lorenzodev12345678 <lorenzodevcritor@gmail.com>
2025-12-23 13:28:43 -0300

    Refactor key verification process in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,29 +1,36 @@
-local OrionLib = loadstring(game:HttpGet('raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
+-- [[ LoreHub v2 - Loader Oficial ]]
+-- [[ Desenvolvido por Lorenzodev12345678 ]]
 
-local ChaveCorreta = "LORE_RLK_2025" -- Sua chave aqui
-local ChaveInserida = ""
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("LoreHub v2 | Verificação", "Midnight")
 
-local KeyWindow = OrionLib:MakeWindow({Name = "LoreHub v2 | Sistema de Key", HidePremium = false, SaveConfig = false})
+-- Criando a aba de Login
+local Tab = Window:NewTab("Sistema de Key")
+local Section = Tab:NewSection("Insira sua Key de RLK")
 
-local KeyTab = KeyWindow:MakeTab({Name = "Verificação", Icon = "rbxassetid://4483345998"})
+_G.Key = "LORE_RLK_2025"
+local KeyInput = ""
 
-KeyTab:AddTextbox({
-    Name = "Insira a Key",
-    Default = "",
-    TextDisappear = true,
-    Callback = function(Value) ChaveInserida = Value end	  
-})
+Section:NewTextbox("Key", "Digite a key oficial aqui", function(txt)
+    KeyInput = txt
+end)
 
-KeyTab:AddButton({
-    Name = "Verificar",
-    Callback = function()
-        if ChaveInserida == ChaveCorreta then
-            OrionLib:MakeNotification({Name = "Sucesso", Content = "Carregando Script...", Time = 2})
-            KeyWindow:Destroy()
-            -- AQUI ESTÁ O SEGREDO: Ele puxa o seu main.lua se a key estiver certa
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/Lorenzodev12345678/LoreHubV2ofc/refs/heads/main/main.lua'))()
-        else
-            OrionLib:MakeNotification({Name = "Erro", Content = "Chave errada, man!", Time = 3})
-        end
-    end    
-})
+Section:NewButton("Verificar Key", "Libera o acesso ao script", function()
+    if KeyInput == _G.Key then
+        -- Aviso de sucesso
+        print("LoreHub: Key correta!")
+        
+        -- Fecha a janela de verificação para abrir o script principal
+        Library:ToggleLib() 
+        
+        -- PUXA O SEU MAIN.LUA ATUALIZADO (KAVO UI)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Lorenzodev12345678/LoreHubV2ofc/main/main.lua'))()
+    else
+        print("LoreHub: Key errada, man!")
+    end
+end)
+
+Section:NewButton("Copiar Link da Key", "Pega o link para gerar a key", function()
+    setclipboard("https://scriptblox.com/script/Universal-Script-LoreahubV2-77644")
+    -- Como a Kavo não tem notificação nativa igual a Orion, usamos o print ou o console
+end)
